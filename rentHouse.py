5f63c8d6329d9a919e6fe1c9f4c41cd181c56cfa
hanlin0721 <a7485888@gmail.com>
2021-03-03 01:15:34 +0800

    增加註解 初步完成登入以及跳到租屋網頁面

--- rentHouse.py
+++ rentHouse.py
@@ -1,14 +1,21 @@
+# coding=UTF-8
 from selenium import webdriver
 from time import sleep
 
+# 設定 driver 實例
 chrome_options = webdriver.ChromeOptions()
 prefs = {"profile.default_content_setting_values.notifications" : 2}
 chrome_options.add_experimental_option("prefs",prefs)
+# 建立 driver 實例
 driver = webdriver.Chrome('./chromedriver', chrome_options=chrome_options)
 
+# 填入 臉書信箱(手機號碼)、密碼
+email = ''
+password = ''
+
 driver.get("https://www.facebook.com/")
-driver.find_element_by_id('email').send_keys('')
-driver.find_element_by_id('pass').send_keys('')
+driver.find_element_by_id('email').send_keys(email)
+driver.find_element_by_id('pass').send_keys(password)
 driver.find_element_by_name('login').click()
 
 sleep(1)
